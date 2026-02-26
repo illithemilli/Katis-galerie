3f06bf2e5d06c7f6e07f317a164e47cfa50b2e43
illithemilli <illithemilli@yahoo.de>
2026-02-26 08:12:08 +0100

    aufräumen

--- script.js
+++
@@ -1,8 +0,0 @@
-function bildOeffnen(src) {
-    document.getElementById("grossesBild").src = src;
-    document.getElementById("lightbox").style.display = "flex";
-}
-
-function schliessen() {
-    document.getElementById("lightbox").style.display = "none";
-}
